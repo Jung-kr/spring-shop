0e3424e38095c9b31292c3c05025826f70befb3c
Jung-kr <jungwy98@gmail.com>
2024-01-05 19:31:42 +0900

    setting : h2설치 & DB 동작확인

---
+++ jpashop/src/test/java/jpabook/jpashop/MemberRepositoryTest.java
@@ -0,0 +1,39 @@
+package jpabook.jpashop;
+
+import org.assertj.core.api.Assert;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class MemberRepositoryTest {
+
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Test
+    @Transactional  //EntityManager를 통한 데이터 변경은 항상 트랜잭션 안에서 이뤄져야하기 때문에 없으면 오류, 테스트 코드에 있는 경우 테스트 종료 후 데이터 롤백
+    @Rollback(false)
+    public void testMember() throws Exception {
+        //given
+        Member member = new Member();
+        member.setUsername("memberA");
+
+        //when
+        Long savedId = memberRepository.save(member);
+        Member findMember = memberRepository.find(savedId);
+
+        //then
+        assertThat(findMember.getId()).isEqualTo(member.getId());
+        assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
+        assertThat(findMember).isEqualTo(member);
+    }
+
+
+}
