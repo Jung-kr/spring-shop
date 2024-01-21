6d0de4cd7d6467ef4db3514689f81177fa1f7443
Jung-kr <jungwy98@gmail.com>
2024-01-21 17:58:18 +0900

    feat : 로그인 & 로그아웃시 홈화면

--- jpashop/src/main/java/jpabook/jpashop/web/HomeController.java
+++ jpashop/src/main/java/jpabook/jpashop/web/HomeController.java
@@ -1,16 +1,26 @@
 package jpabook.jpashop.web;
 
+import jpabook.jpashop.domain.Member;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttribute;
 
 @Controller
 @Slf4j
 public class HomeController {
 
     @RequestMapping("/")
-    public String home() {
-        log.info("home controller");
-        return "home";
+    public String home(@SessionAttribute(name = "loginMember",required = false)Member loginMember, Model model) {
+
+        System.out.println("loginMember = " + loginMember);
+        //세션에 회원 데이터가 없으면
+        if (loginMember == null) {
+            return "home";
+        }
+
+        model.addAttribute("member", loginMember);
+        return "loginHome";
     }
 }
